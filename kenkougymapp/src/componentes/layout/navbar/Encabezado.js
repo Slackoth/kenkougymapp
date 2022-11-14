21970af28abae94e9668e61b7d22a55973fd0329
GuillermoMorales <00011417@uca.edu.sv.com>
2022-11-13 21:12:53 -0600

    Se usa SPA en navbar

--- kenkougymapp/src/componentes/layout/navbar/Encabezado.js
+++ kenkougymapp/src/componentes/layout/navbar/Encabezado.js
@@ -1,22 +1,41 @@
-import { Container, Navbar, Nav } from 'react-bootstrap';
+import { Container, Navbar, Nav } from "react-bootstrap";
+import { Route, NavLink, HashRouter } from "react-router-dom";
+
+import "./nav.css";
 
 const Encabezado = () => {
-    return (
-        <Navbar bg="dark" variant='dark' expand="lg" className='mb-3'>
-            <Container>
-                <Navbar.Brand href="/login">Kenkou Gym</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="/menu">Inicio</Nav.Link>
-                        <Nav.Link href="/recipes">Recetas</Nav.Link>
-                        <Nav.Link href="/rutines">Rutinas</Nav.Link>
-                        <Nav.Link href="/perfil">Progreso</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    );
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg" className="mb-3">
+      <Container>
+        <Navbar.Brand href="/login">Kenkou Gym</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav>
+            <Nav.Link>
+              <NavLink className="blanco" to="/menu">
+                Inicio
+              </NavLink>
+            </Nav.Link>
+            <Nav.Link>
+              <NavLink className="blanco" to="/rutines">
+                Rutinas
+              </NavLink>
+            </Nav.Link>
+            <Nav.Link>
+              <NavLink className="blanco" to="/recipes">
+                Recetas
+              </NavLink>
+            </Nav.Link>
+            <Nav.Link>
+              <NavLink className="blanco" to="/perfil">
+                Progreso
+              </NavLink>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Encabezado;
